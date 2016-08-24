ea8aa851341cd18148534bd8b9649473519e257a
Donghee Lee <jacobdhlee@gmail.com>
2016-08-24 12:40:16 -0400

    Initial Commit

---
+++ Pitch_Perfect_projectTests/Pitch_Perfect_projectTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pitch_Perfect_projectTests.swift
+//  Pitch_Perfect_projectTests
+//
+//  Created by Donghee Lee on 8/24/16.
+//  Copyright © 2016 jacob_lee. All rights reserved.
+//
+
+import XCTest
+@testable import Pitch_Perfect_project
+
+class Pitch_Perfect_projectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
